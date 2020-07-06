a643be8208691d7697e83c7ea9f92403b01e7f92
Dan Weikart <danmweikart@gmail.com>
2020-07-05 19:23:43 -0700

    fixed connection in server.js

--- server.js
+++ server.js
@@ -16,10 +16,7 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(MONGODB_URI, {
-  useNewUrlParser: true,
-  useFindAndModify: false
-});
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true });
 
 // routes
 app.use(require("./routes/api.js"));
